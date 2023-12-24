f147808115f14cd7a0c332f64fccbc680a8d19ca
Logan Mee <117446049+LoganMee@users.noreply.github.com>
2023-12-24 00:35:46 +0000

    Switched to Multi-Channel Audio
    Each ear now uses a separate audio channel for greater control
    Reformatted file for better readability
    Renamed variables and removed unnecessary function inputs

--- python3DAudio.py
+++ python3DAudio.py
@@ -3,13 +3,7 @@ from tkinter import filedialog
 import math
 import pygame
 import os
-
-root = Tk()
-root.title("3D Audio")
-root.geometry("800x600")
-
-pygame.mixer.init()
-
+import time
 
 #User Settings
 userGender = "male"
@@ -19,93 +13,103 @@ if userGender == "male":
 else:
     estimatedHeadCircumference = 41.02 + (0.08673 * userHeight)
 estimatedHeadDiameter = estimatedHeadCircumference/math.pi
-itd = (estimatedHeadDiameter / 100) / 343                       #Interaural timing difference (ITD)
+itd = (estimatedHeadDiameter / 100) / 343   #Interaural timing difference (ITD)
 
 
-#Music Playback
+#Window
+root = Tk()
+root.title("3D Audio")
+root.geometry("800x600")
+
+#Music Playback Variables
 filePath = ""
 fileName = ""
 paused = False
 
-channel = pygame.mixer.find_channel()
-channel.set_volume(0.5, 0.5)
-#channel2 = pygame.mixer.find_channel()
-#channel2.set_volume(0.0, 0.5)
+pygame.mixer.init()
+channel0 = pygame.mixer.Channel(0)
+channel0.set_volume(0.5, 0.0)
+channel1 = pygame.mixer.Channel(1)
+channel1.set_volume(0.0, 0.5)
 
-
+#################### Subroutines ####################
+#Menu subroutines
 def open_file():
     global filePath, fileName
     filePath = filedialog.askopenfilename()
     fileName = os.path.basename(filePath)
 
+#Button subroutines
 def play_music():
-    global paused, filePath, currentFile, fileName
+    global paused, filePath, playbackText, fileName
     if not paused:
-        channel.play(pygame.mixer.Sound(filePath))
-        currentFile.config(text=f"Playing: {fileName}")
-        #channel2.play(pygame.mixer.Sound(filePath))
+        channel0.play(pygame.mixer.Sound(filePath))
+        channel1.play(pygame.mixer.Sound(filePath))
+        playbackText.config(text=f"Playing: {fileName}")
     else:
         pygame.mixer.unpause()
         paused = False
-        currentFile.config(text=f"Playing: {fileName}")
+        playbackText.config(text=f"Playing: {fileName}")
 
 def pause_music():
-    global paused, currentFile
+    global paused, playbackText
     pygame.mixer.pause()
     paused = True
-    currentFile.config(text="Paused")
+    playbackText.config(text="Paused")
 
-
-#Audio Simulation Visualisation
-radius = 25
-canvasWidth = 500
-canvasHeight = 500
-canvasCentre = [canvasWidth/2, canvasHeight/2]
-
-canvas = Canvas(root, width=canvasWidth, height=canvasHeight, bg="white")
-canvas.pack()
-head = canvas.create_oval(canvasCentre[0]-radius, canvasCentre[1]-radius, canvasCentre[0]+radius, canvasCentre[1]+radius, outline="black", fill="wheat", width=2)
-audioSource = canvas.create_oval(canvasCentre[0]-radius/2, canvasCentre[1]-radius*2, canvasCentre[0]+radius/2, canvasCentre[1]-radius, outline="black", fill="Grey", width=2)
-canvas.pack()
-
+#Canvas subroutines
 def distanceBetweenPoints(x1,y1,x2,y2):
     changeInX = x2 - x1
     changeInY = y2 - y1
     distance = math.sqrt(changeInX**2 + changeInY**2)
     return distance
 
-def angleBetweenPoints(x1,y1,x2,y2, hypotenuse):
+def angleBetweenPoints(x1, x2, hypotenuse):
     changeInX = x2 - x1
-    changeInY = y2 - y1
-    angle = math.asin(changeInX/hypotenuse) # gets angle in radians
+    angle = math.asin(changeInX/hypotenuse) #Gets angle in radians
     return angle
 
 def move(event):
-    global audioSource, radius, canvasCentre, channel, channel2
-    canvas.moveto(audioSource, event.x - radius/2, event.y - radius/2)
+    global audioSource, radius
+    newX = event.x
+    newY = event.y
+    canvas.moveto(audioSource, newX - radius/2, newY - radius/2)
+    volumeChange(newX, newY)
 
-    distance = distanceBetweenPoints(canvasCentre[0], canvasCentre[1], event.x, event.y)
+def volumeChange(newX, newY):
+    global canvasCentre, channel0, channel1
+
+    distance = distanceBetweenPoints(canvasCentre[0], canvasCentre[1], newX, newY)
     distanceScaled = distance/25
     if distanceScaled > 1:
         intensityMultiplier = 1/(distanceScaled**2)
     else:
         intensityMultiplier = 1
-    #print(intensityMultiplier)
 
-    angle = angleBetweenPoints(canvasCentre[0], canvasCentre[1], event.x, event.y, distance)
-    #print(angle)
+    angle = angleBetweenPoints(canvasCentre[0], newX, distance)
 
     leftIntensity = ((math.sin(angle-math.pi)/2)+0.5) * intensityMultiplier
     rightIntensity = ((math.sin(angle)/2)+0.5) * intensityMultiplier
-    print(leftIntensity)
+    print(leftIntensity, rightIntensity)
     
-    pygame.mixer.Channel(0).set_volume(leftIntensity, rightIntensity)
-    #pygame.mixer.Channel(1).set_volume(0.0, rightIntensity)
+    channel0.set_volume(leftIntensity, 0.0)
+    channel1.set_volume(0.0, rightIntensity)
+
+#################### GUI ####################
+#Canvas for Audio Simulation Visualisation
+radius = 25
+canvasWidth = 500
+canvasHeight = 500
+canvasCentre = [canvasWidth/2, canvasHeight/2]
+
+canvas = Canvas(root, width=canvasWidth, height=canvasHeight, bg="white")
+head = canvas.create_oval(canvasCentre[0]-radius, canvasCentre[1]-radius, canvasCentre[0]+radius, canvasCentre[1]+radius, outline="black", fill="wheat", width=2)
+audioSource = canvas.create_oval(canvasCentre[0]-radius/2, canvasCentre[1]-radius*2, canvasCentre[0]+radius/2, canvasCentre[1]-radius, outline="black", fill="Grey", width=2)
+canvas.pack()
 
 canvas.bind('<B1-Motion>', move)
 
-
-#menus & buttons
+#Menu
 menubar = Menu(root)
 root.config(menu=menubar)    
 
@@ -113,9 +117,10 @@ file_menu = Menu(menubar, tearoff=False)
 menubar.add_cascade(label="File", menu=file_menu)
 file_menu.add_command(label="Open", command=open_file)
 
-currentFile = Label(root, text = "No song playing")
-currentFile.pack()
+playbackText = Label(root, text = "No song playing")
+playbackText.pack()
 
+#Buttons
 playButtonImg = PhotoImage(file="play.png")
 pauseButtonImg = PhotoImage(file="pause.png")
 
@@ -126,5 +131,5 @@ pauseButton = Button(control_frame, image=pauseButtonImg, borderwidth=0, command
 playButton.grid(row=0, column=0, padx=7, pady=10)
 pauseButton.grid(row=0, column=1, padx=7, pady=10)
 
-
+#Loop
 root.mainloop()
