bf390ecf543cba076db723f8e373135a9754a0aa
Logan Mee <117446049+LoganMee@users.noreply.github.com>
2024-03-25 21:56:41 +0000

    OOP rewrite
    Rewritten code in OOP format
    Standardised variable names to use camelCase
    Using 100% grid statements instead of pack

--- python3DAudio.py
+++ python3DAudio.py
@@ -15,50 +15,7 @@ else:
 estimatedHeadDiameter = estimatedHeadCircumference/math.pi
 itd = (estimatedHeadDiameter / 100) / 343   #Interaural timing difference (ITD)
 
-
-#Window
-root = Tk()
-root.title("3D Audio")
-root.geometry("800x600")
-
-#Music Playback Variables
-filePath = ""
-fileName = ""
-paused = False
-minimumVolume = 0.03 #minimum volume at 0 distance
-
-pygame.mixer.init()
-channel0 = pygame.mixer.Channel(0) #left1
-channel0.set_volume(0.5, 0.0)
-channel1 = pygame.mixer.Channel(1) #right1
-channel1.set_volume(0.0, 0.5)
-
-#################### Subroutines ####################
-#Menu subroutines
-def open_file():
-    global filePath, fileName
-    filePath = filedialog.askopenfilename()
-    fileName = os.path.basename(filePath)
-
-#Button subroutines
-def play_music():
-    global paused, filePath, playbackText, fileName
-    if not paused:
-        channel0.play(pygame.mixer.Sound(filePath))
-        channel1.play(pygame.mixer.Sound(filePath))
-        playbackText.config(text=f"Playing: {fileName}")
-    else:
-        pygame.mixer.unpause()
-        paused = False
-        playbackText.config(text=f"Playing: {fileName}")
-
-def pause_music():
-    global paused, playbackText
-    pygame.mixer.pause()
-    paused = True
-    playbackText.config(text="Paused")
-
-#Canvas subroutines
+#mathmatical Functions
 def distanceBetweenPoints(x1,y1,x2,y2):
     changeInX = x2 - x1
     changeInY = y2 - y1
@@ -70,74 +27,128 @@ def angleBetweenPoints(x1, x2, hypotenuse):
     angle = math.asin(changeInX/hypotenuse) #Gets angle in radians
     return angle
 
-def move(event):
-    global audioSource, radius
-    newX = event.x
-    newY = event.y
-    canvas.moveto(audioSource, newX - radius/2, newY - radius/2)
-    volumeChange(newX, newY)
 
-def volumeChange(newX, newY):
-    global canvasCentre, minimumVolume, channel0, channel1
 
-    distance = distanceBetweenPoints(canvasCentre[0], canvasCentre[1], newX, newY)
-    distanceScaled = distance/25
-    if distanceScaled > 1:
-        intensityMultiplier = 1/(distanceScaled**2)
-    else:
-        intensityMultiplier = 1
+class Audio3DInterface:
 
-    angle = angleBetweenPoints(canvasCentre[0], newX, distance)
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("3D Audio")
+        self.root.geometry("800x700")
+        
+        self.mainFrame = Frame(self.root)
+        self.mainFrame.grid(column=0, row=0, padx=10, pady=10)
+        self.controlsFrame = Frame(self.root)
+        self.controlsFrame.grid(column=0, row=1, padx=10, pady=10)
 
-    leftIntensity = ((math.sin(angle-math.pi)/2)+0.5) * intensityMultiplier
-    rightIntensity = ((math.sin(angle)/2)+0.5) * intensityMultiplier
+        self.filePath = ""
+        self.fileName = ""
+        self.paused = False
+        self.minimumVolume = 0.03
 
-    if leftIntensity < minimumVolume:
-        leftIntensity = minimumVolume * intensityMultiplier #(minimum volume level)
+        pygame.mixer.init()
+        self.channel0 = pygame.mixer.Channel(0) #left1
+        self.channel0.set_volume(0.5, 0.0)
+        self.channel1 = pygame.mixer.Channel(1) #right1
+        self.channel1.set_volume(0.0, 0.5)
+
+        self.playButtonImg = PhotoImage(file="play.png")
+        self.pauseButtonImg = PhotoImage(file="pause.png")
     
-    if rightIntensity < minimumVolume:
-        rightIntensity = minimumVolume * intensityMultiplier #(minimum volume level)
-
-    print(leftIntensity, rightIntensity)
+    def run(self):
+        self.createWidgets()
+        self.root.mainloop()
     
-    channel0.set_volume(leftIntensity, 0.0)
-    channel1.set_volume(0.0, rightIntensity)
+    #################### Subroutines ####################
+    #Menu subroutines
+    def openFile(self):
+        self.filePath = filedialog.askopenfilename()
+        self.fileName = os.path.basename(self.filePath)
 
-#################### GUI ####################
-#Canvas for Audio Simulation Visualisation
-radius = 25
-canvasWidth = 500
-canvasHeight = 500
-canvasCentre = [canvasWidth/2, canvasHeight/2]
+    #Button subroutines
+    def playMusic(self, playbackText):
+        if not self.paused:
+            self.channel0.play(pygame.mixer.Sound(self.filePath))
+            self.channel1.play(pygame.mixer.Sound(self.filePath))
+            playbackText.config(text=f"Playing: {self.fileName}")
+        else:
+            pygame.mixer.unpause()
+            self.paused = False
+            playbackText.config(text=f"Playing: {self.fileName}")
 
-canvas = Canvas(root, width=canvasWidth, height=canvasHeight, bg="white")
-head = canvas.create_oval(canvasCentre[0]-radius, canvasCentre[1]-radius, canvasCentre[0]+radius, canvasCentre[1]+radius, outline="black", fill="wheat", width=2)
-audioSource = canvas.create_oval(canvasCentre[0]-radius/2, canvasCentre[1]-radius*2, canvasCentre[0]+radius/2, canvasCentre[1]-radius, outline="black", fill="Grey", width=2)
-canvas.pack()
+    def pauseMusic(self, playbackText):
+        pygame.mixer.pause()
+        self.paused = True
+        playbackText.config(text="Paused")
 
-canvas.bind('<B1-Motion>', move)
+    #Canvas subroutines
+    def move(self, event, audioSource, radius, canvas, canvasCentre):
+        newX = event.x
+        newY = event.y
+        canvas.moveto(audioSource, newX - radius/2, newY - radius/2)
+        self.volumeChange(newX, newY, canvasCentre)
 
-#Menu
-menubar = Menu(root)
-root.config(menu=menubar)    
+    def volumeChange(self, newX, newY, canvasCentre):
+        distance = distanceBetweenPoints(canvasCentre[0], canvasCentre[1], newX, newY)
+        distanceScaled = distance/25
+        if distanceScaled > 1:
+            intensityMultiplier = 1/(distanceScaled**2)
+        else:
+            intensityMultiplier = 1
 
-file_menu = Menu(menubar, tearoff=False)
-menubar.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="Open", command=open_file)
+        angle = angleBetweenPoints(canvasCentre[0], newX, distance)
 
-playbackText = Label(root, text = "No song playing")
-playbackText.pack()
+        leftIntensity = ((math.sin(angle-math.pi)/2)+0.5) * intensityMultiplier
+        rightIntensity = ((math.sin(angle)/2)+0.5) * intensityMultiplier
 
-#Buttons
-playButtonImg = PhotoImage(file="play.png")
-pauseButtonImg = PhotoImage(file="pause.png")
+        if leftIntensity < self.minimumVolume:
+            leftIntensity = self.minimumVolume * intensityMultiplier #(minimum volume level)
+        
+        if rightIntensity < self.minimumVolume:
+            rightIntensity = self.minimumVolume * intensityMultiplier #(minimum volume level)
 
-control_frame = Frame(root)
-control_frame.pack()
-playButton = Button(control_frame, image=playButtonImg, borderwidth=0, command=play_music)
-pauseButton = Button(control_frame, image=pauseButtonImg, borderwidth=0, command=pause_music)
-playButton.grid(row=0, column=0, padx=7, pady=10)
-pauseButton.grid(row=0, column=1, padx=7, pady=10)
+        print(leftIntensity, rightIntensity)
+        
+        self.channel0.set_volume(leftIntensity, 0.0)
+        self.channel1.set_volume(0.0, rightIntensity)
 
-#Loop
-root.mainloop()
+    def createWidgets(self):
+        #################### GUI ####################
+        #Canvas for Audio Simulation Visualisation
+        radius = 25
+        canvasWidth = 500
+        canvasHeight = 500
+        canvasCentre = [canvasWidth/2, canvasHeight/2]
+
+        canvas = Canvas(self.mainFrame, width=canvasWidth, height=canvasHeight, bg="white")
+        head = canvas.create_oval(canvasCentre[0]-radius, canvasCentre[1]-radius, canvasCentre[0]+radius, canvasCentre[1]+radius, outline="black", fill="wheat", width=2)
+        audioSource = canvas.create_oval(canvasCentre[0]-radius/2, canvasCentre[1]-radius*2, canvasCentre[0]+radius/2, canvasCentre[1]-radius, outline="black", fill="Grey", width=2)
+        canvas.grid(column=0, row=0)
+
+        canvas.bind('<B1-Motion>', lambda event:self.move(event, audioSource, radius, canvas, canvasCentre))
+
+        #Menu
+        menubar = Menu(self.root)
+        self.root.config(menu=menubar)    
+
+        fileMenu = Menu(menubar, tearoff=False)
+        menubar.add_cascade(label="File", menu=fileMenu)
+        fileMenu.add_command(label="Open", command=self.openFile)
+
+        #Info Bar
+        playbackText = Label(self.controlsFrame, text = "No song playing")
+        playbackText.grid(column=0, row=0, columnspan=2)
+
+        #Buttons
+        playButton = Button(self.controlsFrame, image=self.playButtonImg, borderwidth=0, command=lambda:self.playMusic(playbackText))
+        pauseButton = Button(self.controlsFrame, image=self.pauseButtonImg, borderwidth=0, command=lambda:self.pauseMusic(playbackText))
+        playButton.grid(row=1, column=0, padx=7, pady=10)
+        pauseButton.grid(row=1, column=1, padx=7, pady=10)
+
+
+
+def main():
+    audioSys1 = Audio3DInterface()
+    audioSys1.run()
+
+main()
